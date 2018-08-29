d310ba869440a701a6f4032e32c1a7f92ed822b9
Mark Gerrard <gerrardmark@gmail.com>
2018-08-29 11:51:54 +0100

    @markgerrard styling

--- src/script/calculator.vue
+++ src/script/calculator.vue
@@ -1,15 +1,25 @@
 <template lang="html">
-  <div id="app">
+  <div class="calculator">
     <div class="calculator__value">{{ input.join(' ') }}</div>
     <main class="calculator__numbers">
-      <CalButton v-for="v in numbers" @clicked="clicked" v-bind:value="v.toString()" />
+      <CalButton @clicked="clicked" value="C" class="calculator__clear" />
+      <CalButton @clicked="clicked" value="7" />
+      <CalButton @clicked="clicked" value="8" />
+      <CalButton @clicked="clicked" value="9" />
+      <CalButton @clicked="clicked" value="4" />
+      <CalButton @clicked="clicked" value="5" />
+      <CalButton @clicked="clicked" value="6" />
+      <CalButton @clicked="clicked" value="3" />
+      <CalButton @clicked="clicked" value="2" />
+      <CalButton @clicked="clicked" value="1" />
+      <CalButton @clicked="clicked" value="0" class="calculator__zero" />
+      <CalButton @clicked="clicked" value="." />
     </main>
-    <aside>
+    <aside class="calculator__operators">
       <CalButton @clicked="clicked" value="+" />
       <CalButton @clicked="clicked" value="-" />
       <CalButton @clicked="clicked" value="/" />
       <CalButton @clicked="clicked" value="*" />
-      <CalButton @clicked="clicked" value="." />
       <CalButton @clicked="clicked" value="=" />
     </aside>
   </div>
@@ -86,4 +96,55 @@ function calculate(str) {
     margin: 0
     padding: 0
     font-family: sans-serif
+
+  .calculator
+    display: grid
+    max-width: 450px
+    grid-template-columns: 25% 25% 25% 25%
+    font-size: 2rem
+    border-radius: .5rem
+    overflow: hidden
+    border: 1px solid #333
+
+    &__value
+      text-align: right
+      padding: 1rem 3rem
+
+    &__numbers
+      display: grid
+      grid-column-start: 1
+      grid-column-end: 4
+      grid-template-columns: 33.33% 33.33% 33.33%
+      grid-template-rows: 25% 25% 25% 25%
+      border-top: 1px solid #AEAEAE
+      border-left: @border-top
+
+    .calculator__button
+      background: #EFEFEF
+      border: none
+      border-right: 1px solid #AEAEAE
+      border-bottom: @border-right
+      font-size: inherit
+      padding: 1rem 2rem
+
+      &:focus
+        background: #DFDFDF
+        outline: none
+
+    &__clear
+      grid-column-end: span 3
+
+    &__zero
+      grid-column-end: span 2
+      text-align: left
+
+    &__operators
+      display: grid
+
+      .calculator__button
+        background-color: orange
+        color: #FFF
+
+    &__value
+      grid-column-end: span 4
 </style>
